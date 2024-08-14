4a9cd664dccb8abca5df5b652c9b023dd63e50f3
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2024-08-14 13:58:49 +0200

    Initial commit

---
+++ webapp/controller/Main.controller.ts
@@ -0,0 +1,12 @@
+import MessageBox from "sap/m/MessageBox";
+import BaseController from "./BaseController";
+
+/**
+ * @namespace com.myorg.myapp.controller
+ */
+export default class Main extends BaseController {
+	public sayHello(): void {
+		MessageBox.show("Hello World!");
+		const core = sap.ui.getCore()
+	}
+}
